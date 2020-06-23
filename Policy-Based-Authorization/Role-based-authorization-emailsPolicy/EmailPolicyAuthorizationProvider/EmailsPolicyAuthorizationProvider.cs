ae1d61cebe18a3313890cbd4940c68b6487d9494
voltwu <h.vigour.wu@gmail.com>
2020-06-23 10:47:43 +0800

    add email policy example

---
+++ Policy-Based-Authorization/Role-based-authorization-emailsPolicy/EmailPolicyAuthorizationProvider/EmailsPolicyAuthorizationProvider.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace authBasics.EmailPolicyAuthorizationProvider
+{
+    public class EmailsRequireHandler : AuthorizationHandler<EmailsRequirement>
+    {
+        protected override Task HandleRequirementAsync(
+            AuthorizationHandlerContext context, 
+            EmailsRequirement requirement)
+        {
+            List<String> userAllClaims = 
+                context.User.Claims.Select(s=>s.Type).ToList();
+
+            //check is if  userAllClaims contains all requirement items.
+            bool containsAll = !requirement.emailList.Except<String>(userAllClaims).Any();
+
+            if (containsAll) 
+            {
+                context.Succeed(requirement);  
+            }
+            return Task.CompletedTask;
+        }
+    }
+    public class EmailsRequirement 
+        : IAuthorizationRequirement {
+        public List<String> emailList { get; }
+
+        public EmailsRequirement(params string[] emails) {
+            this.emailList = new List<string>(emails);
+        }
+    }
+}
---
+++ Policy-Based-Authorization/Role-based-authorization-emailsPolicy/Migrations/20200619044850_changeEmails.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace authBasics.Migrations
+{
+    public partial class changeEmails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Gmail",
+                table: "users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Outlook",
+                table: "users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "QQ",
+                table: "users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gmail",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "Outlook",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "QQ",
+                table: "users");
+        }
+    }
+}
